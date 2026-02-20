130c8720a1527314acc41c1762d981bbcdd8edfd
DOC <nushajon@gmail.com>
2026-02-20 17:25:48 +0500

    url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "yalla-co-menu-beckend-dev-47c9.twc1.net/api";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://yalla-co-menu-beckend-dev-47c9.twc1.net/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
